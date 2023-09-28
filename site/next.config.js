5d5c35d6d6fef0db281a9a44b44fa54708e9c739
tanishapatil1234 <111611921+tanishapatil1234@users.noreply.github.com>
2023-09-28 01:33:38 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'Kasm Registry',
+    description: 'My List of Kasm Workspaces',
+    icon: 'https://tanishapatil1234.github.io/kasm-registry/1.0/image.png',
+    listUrl: 'https://tanishapatil1234.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/tanishapatil1234/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
